cb181116e7bef31d6e268e11ef08d52774e858bf
Luke Matteoni <ljmatteoni@users.noreply.github.com>
2022-10-22 09:32:08 -0500

    Create first.py

---
+++ first.py
@@ -0,0 +1,15 @@
+print ("hello world")
+
+bog = input (" ") 
+
+print (bog)
+
+print (1+1)
+
+big = int(input (" "))
+
+print (1 + big)
+
+boog = ['one', 'two','three','four','five']
+
+print(boog[0].title())
